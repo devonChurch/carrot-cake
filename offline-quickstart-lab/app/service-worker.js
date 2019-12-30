bad57147268ac826c524548b7151ff49c5055e10
devonChurch <notifydevon@gmail.com>
2019-12-31 11:17:33 +1300

    feat: 🎸 complete "offline" lab

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,1 +1,42 @@
+const cacheName = 'cache-v2';
+const precacheResources = [
+  '/',
+  'index.html',
+  'styles/main.css',
+  'images/space1.jpg',
+  'images/space2.jpg',
+  'images/space3.jpg'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+      try {
+        const cache = await caches.open(cacheName);
+        return Promise.all(precacheResources.map(asset => cache.add(asset)));
+      } catch (error) {
+        console.error(error);
+      }
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    try {
+      const cache = await caches.open(cacheName);
+      const cacheResponse = await cache.match(event.request.url);
+      if (cacheResponse) {
+        return cacheResponse;
+      } else {
+        const serverResponse = await fetch(event.request.url);
+        if (!serverResponse.ok) {
+          throw new Error(serverResponse.statusText);
+        }
+        cache.add(serverResponse.clone());
+        return serverResponse;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+  })())
+})
